f3df2a6865c45235af9b552c7ae8262d2171b443
johntiger1 <john.person1@gmail.com>
2018-08-12 01:14:08 -0400

    add initial site

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import title_pic from "../images/ccia.jpg"
+const IndexPage = () => (
+
+	<div>
+
+
+
+
+
+<img src={title_pic} alt="Main hall of CCIA building"/> 
+
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+
+
+	</div>
+
+)
+
+export default IndexPage
