c01ee81f7b1631566a7a575b6a18b6fa2744fed9
johntiger1 <john.person1@gmail.com>
2018-08-12 23:24:35 -0400

    add ga code

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,24 @@ module.exports = {
   siteMetadata: {
     title: 'Chinese Canadian Intercultural Association',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+
+{
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-123749137-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+  ],
+
+
+
 }
